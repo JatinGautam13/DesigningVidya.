efc270b00bb179dcc0fbf6984231f9a7ec7dc2d3
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2026-01-09 11:50:35 +0000

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,8 +39,39 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17855679185"
+          strategy="beforeInteractive"
+        />
+        <Script id="gtag-init" strategy="beforeInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-17855679185');`}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W96Q8L59');`}
+        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} app-shell`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W96Q8L59"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <a href="#main-content" className="skip-link">
           Skip to main content
         </a>
